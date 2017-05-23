77236c9606182757cfe652575ceb19c89d1f8e7f
Peter Andersson <petoj87@gmail.com>
2017-05-23 13:55:36 +0200

    Removed unneeded requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="pytrafikverket",
-    version="0.1.3",
+    version="0.1.4",
     description="api for trafikverket in sweden",
     url='https://github.com/AnderssonPeter/pytrafikverket',
     author="Peter Andersson",
