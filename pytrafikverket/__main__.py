e5b7a63260b90a9f85fbbbc772c5b4fb52dc2c0d
endor-force <1937941+endor-force@users.noreply.github.com>
2018-08-19 16:46:08 +0200

    Added support for weather sensors
    Added support to retreive weather sensor data from trafikverket road sensors.
    Bumped version from 1.5.7 to 1.5.8

--- pytrafikverket/__main__.py
+++ pytrafikverket/__main__.py
@@ -4,11 +4,13 @@ import asyncio
 import aiohttp
 import async_timeout
 from pytrafikverket.trafikverket_train import TrafikverketTrain
+from pytrafikverket.trafikverket_weather import TrafikverketWeather
 from pytrafikverket.trafikverket import Trafikverket
 
 SEARCH_FOR_STATION = "search-for-station"
 GET_TRAIN_STOP = "get-train-stop"
 GET_NEXT_TRAIN_STOP = "get-next-train-stop"
+GET_WEATHER = "get-weather"
 
 async def async_main(loop):
     async with aiohttp.ClientSession(loop=loop) as session:
@@ -17,7 +19,8 @@ async def async_main(loop):
         parser.add_argument("-key", type=str)
         parser.add_argument("-method", choices=(SEARCH_FOR_STATION,
                                                 GET_TRAIN_STOP,
-                                                GET_NEXT_TRAIN_STOP))
+                                                GET_NEXT_TRAIN_STOP,
+                                                GET_WEATHER))
         parser.add_argument("-station", type=str)
         parser.add_argument("-from-station", type=str)
         parser.add_argument("-to-station", type=str)
@@ -26,6 +29,7 @@ async def async_main(loop):
         args = parser.parse_args()
 
         train_api = TrafikverketTrain(session, args.key)
+        weather_api = TrafikverketWeather(session, args.key)
         with async_timeout.timeout(10):
             if args.method == SEARCH_FOR_STATION:
                 if args.station is None:
@@ -62,6 +66,17 @@ async def async_main(loop):
                       "estimated time: %s, time: %s, state: %s" %
                       (train_stop.estimated_time_at_location, train_stop.time_at_location,
                        train_stop.get_state()))
+            elif args.method == GET_WEATHER:
+                if args.station is None:
+                    raise ValueError("-station is required with name of Weather station (ex. -station \"Nöbbele\")")
+                weather = await weather_api.async_get_weather(args.station)
+                print("Name: %s, id: %s, road temp: %s , air temp: %s, " %
+                      (weather.station_name, weather.station_id, weather.road_temp, weather.air_temp) + 
+                      "humidity: %s, precipitation: %s, wind direction %s degrees / " %
+                      (weather.humidity, weather.precipitationtype, weather.winddirection) +
+                      "%s, wind force: %s m/s (10 min avg)" %
+                      (weather.winddirectiontext, weather.windforce))
+
 
 
 def main():
