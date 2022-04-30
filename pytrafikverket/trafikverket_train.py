26fc651f2dd263d2b4cad02dec5c1dabbd6765c1
G Johansson <goran.johansson@shiftit.se>
2022-04-30 18:42:45 +0200

    Fix other info

--- pytrafikverket/trafikverket_train.py
+++ pytrafikverket/trafikverket_train.py
@@ -5,8 +5,14 @@ from enum import Enum
 
 import aiohttp
 from pytrafikverket.trafikverket import (
-    FieldFilter, FieldSort, FilterOperation, NodeHelper, OrFilter, SortOrder,
-    Trafikverket)
+    FieldFilter,
+    FieldSort,
+    FilterOperation,
+    NodeHelper,
+    OrFilter,
+    SortOrder,
+    Trafikverket,
+)
 
 
 class StationInfo(object):
@@ -25,12 +31,9 @@ class StationInfo(object):
         """Map station information in XML data."""
         node_helper = NodeHelper(node)
         location_signature = node_helper.get_text("LocationSignature")
-        advertised_location_name = \
-            node_helper.get_text("AdvertisedLocationName")
+        advertised_location_name = node_helper.get_text("AdvertisedLocationName")
         location_advertised = node_helper.get_text("Advertised")
-        return cls(location_signature,
-                   advertised_location_name,
-                   location_advertised)
+        return cls(location_signature, advertised_location_name, location_advertised)
 
 
 class TrainStopStatus(Enum):
@@ -44,17 +47,28 @@ class TrainStopStatus(Enum):
 class TrainStop(object):
     """Contain information about a train stop."""
 
-    _required_fields = ["ActivityId", "Canceled", "AdvertisedTimeAtLocation",
-                        "EstimatedTimeAtLocation", "TimeAtLocation",
-                        "OtherInformation", "Deviation", "ModifiedTime"]
+    _required_fields = [
+        "ActivityId",
+        "Canceled",
+        "AdvertisedTimeAtLocation",
+        "EstimatedTimeAtLocation",
+        "TimeAtLocation",
+        "OtherInformation",
+        "Deviation",
+        "ModifiedTime",
+    ]
 
-    def __init__(self, id, canceled: bool,
-                 advertised_time_at_location: datetime,
-                 estimated_time_at_location: datetime,
-                 time_at_location: datetime,
-                 other_information: typing.List[str],
-                 deviations: typing.List[str],
-                 modified_time: datetime):
+    def __init__(
+        self,
+        id,
+        canceled: bool,
+        advertised_time_at_location: datetime,
+        estimated_time_at_location: datetime,
+        time_at_location: datetime,
+        other_information: typing.List[str],
+        deviations: typing.List[str],
+        modified_time: datetime,
+    ):
         """Initialize TrainStop."""
         self.id = id
         self.canceled = canceled
@@ -69,14 +83,17 @@ class TrainStop(object):
         """Retrieve the state of the departure."""
         if self.canceled:
             return TrainStopStatus.canceled
-        if (self.advertised_time_at_location is not None and
-                self.time_at_location is not None and
-                self.advertised_time_at_location != self.time_at_location):
+        if (
+            self.advertised_time_at_location is not None
+            and self.time_at_location is not None
+            and self.advertised_time_at_location != self.time_at_location
+        ):
             return TrainStopStatus.delayed
-        if (self.advertised_time_at_location is not None and
-                self.estimated_time_at_location is not None and
-                self.advertised_time_at_location !=
-                self.estimated_time_at_location):
+        if (
+            self.advertised_time_at_location is not None
+            and self.estimated_time_at_location is not None
+            and self.advertised_time_at_location != self.estimated_time_at_location
+        ):
             return TrainStopStatus.delayed
         return TrainStopStatus.on_time
 
@@ -84,16 +101,18 @@ class TrainStop(object):
         """Calculate the delay of a departure."""
         if self.canceled:
             return None
-        if (self.advertised_time_at_location is not None and
-                self.time_at_location is not None and
-                self.advertised_time_at_location != self.time_at_location):
+        if (
+            self.advertised_time_at_location is not None
+            and self.time_at_location is not None
+            and self.advertised_time_at_location != self.time_at_location
+        ):
             return self.time_at_location - self.advertised_time_at_location
-        if (self.advertised_time_at_location is not None and
-                self.estimated_time_at_location is not None and
-                self.advertised_time_at_location !=
-                self.estimated_time_at_location):
-            return self.estimated_time_at_location - \
-                self.advertised_time_at_location
+        if (
+            self.advertised_time_at_location is not None
+            and self.estimated_time_at_location is not None
+            and self.advertised_time_at_location != self.estimated_time_at_location
+        ):
+            return self.estimated_time_at_location - self.advertised_time_at_location
         return None
 
     @classmethod
@@ -103,16 +122,23 @@ class TrainStop(object):
         activity_id = node_helper.get_text("ActivityId")
         canceled = node_helper.get_bool("Canceled")
         advertised_time_at_location = node_helper.get_datetime(
-            "AdvertisedTimeAtLocation")
-        estimated_time_at_location = node_helper.get_datetime(
-            "EstimatedTimeAtLocation")
+            "AdvertisedTimeAtLocation"
+        )
+        estimated_time_at_location = node_helper.get_datetime("EstimatedTimeAtLocation")
         time_at_location = node_helper.get_datetime("TimeAtLocation")
-        other_information = node_helper.get_texts("OtherInformation")
+        other_information = node_helper.get_texts("OtherInformation/Description")
         deviations = node_helper.get_texts("Deviation")
         modified_time = node_helper.get_datetime_for_modified("ModifiedTime")
-        return cls(activity_id, canceled, advertised_time_at_location,
-                   estimated_time_at_location, time_at_location,
-                   other_information, deviations, modified_time)
+        return cls(
+            activity_id,
+            canceled,
+            advertised_time_at_location,
+            estimated_time_at_location,
+            time_at_location,
+            other_information,
+            deviations,
+            modified_time,
+        )
 
 
 class TrafikverketTrain(object):
@@ -128,32 +154,37 @@ class TrafikverketTrain(object):
             "TrainStation",
             "1.4",
             StationInfo._required_fields,
-            [FieldFilter(FilterOperation.equal,
-                         "AdvertisedLocationName", location_name),
-             FieldFilter(FilterOperation.equal, "Advertised", "true")])
+            [
+                FieldFilter(
+                    FilterOperation.equal, "AdvertisedLocationName", location_name
+                ),
+                FieldFilter(FilterOperation.equal, "Advertised", "true"),
+            ],
+        )
         if len(train_stations) == 0:
-            raise ValueError(
-                "Could not find a station with the specified name")
+            raise ValueError("Could not find a station with the specified name")
         if len(train_stations) > 1:
-            raise ValueError(
-                "Found multiple stations with the specified name")
+            raise ValueError("Found multiple stations with the specified name")
 
         return StationInfo.from_xml_node(train_stations[0])
 
     async def async_search_train_stations(
-            self, location_name: str) -> typing.List[StationInfo]:
+        self, location_name: str
+    ) -> typing.List[StationInfo]:
         """Search for train stations."""
         train_stations = await self._api.async_make_request(
             "TrainStation",
             "1.4",
-            ["AdvertisedLocationName", "LocationSignature",
-             "Advertised", "Deleted"],
-            [FieldFilter(FilterOperation.like,
-                         "AdvertisedLocationName", location_name),
-             FieldFilter(FilterOperation.equal, "Advertised", "true")])
+            ["AdvertisedLocationName", "LocationSignature", "Advertised", "Deleted"],
+            [
+                FieldFilter(
+                    FilterOperation.like, "AdvertisedLocationName", location_name
+                ),
+                FieldFilter(FilterOperation.equal, "Advertised", "true"),
+            ],
+        )
         if len(train_stations) == 0:
-            raise ValueError(
-                "Could not find a station with the specified name")
+            raise ValueError("Could not find a station with the specified name")
 
         result = [StationInfo] * 0
 
@@ -163,30 +194,41 @@ class TrafikverketTrain(object):
         return result
 
     async def async_get_train_stop(
-            self, from_station: StationInfo,
-            to_station: StationInfo, time_at_location: datetime) -> TrainStop:
+        self,
+        from_station: StationInfo,
+        to_station: StationInfo,
+        time_at_location: datetime,
+    ) -> TrainStop:
         """Retrieve the train stop."""
         date_as_text = time_at_location.strftime(Trafikverket.date_time_format)
 
-        filters = [FieldFilter(FilterOperation.equal,
-                   "ActivityType", "Avgang"),
-                   FieldFilter(FilterOperation.equal,
-                               "LocationSignature",
-                               from_station.signature),
-
-                   FieldFilter(FilterOperation.equal,
-                               "AdvertisedTimeAtLocation",
-                               date_as_text),
-
-                   OrFilter([FieldFilter(FilterOperation.equal,
-                                         "ViaToLocation.LocationName",
-                                         to_station.signature),
-                             FieldFilter(FilterOperation.equal,
-                                         "ToLocation.LocationName",
-                                         to_station.signature)])]
+        filters = [
+            FieldFilter(FilterOperation.equal, "ActivityType", "Avgang"),
+            FieldFilter(
+                FilterOperation.equal, "LocationSignature", from_station.signature
+            ),
+            FieldFilter(
+                FilterOperation.equal, "AdvertisedTimeAtLocation", date_as_text
+            ),
+            OrFilter(
+                [
+                    FieldFilter(
+                        FilterOperation.equal,
+                        "ViaToLocation.LocationName",
+                        to_station.signature,
+                    ),
+                    FieldFilter(
+                        FilterOperation.equal,
+                        "ToLocation.LocationName",
+                        to_station.signature,
+                    ),
+                ]
+            ),
+        ]
 
         train_announcements = await self._api.async_make_request(
-            "TrainAnnouncement", "1.6", TrainStop._required_fields, filters)
+            "TrainAnnouncement", "1.6", TrainStop._required_fields, filters
+        )
 
         if len(train_announcements) == 0:
             raise ValueError("No TrainAnnouncement found")
@@ -198,39 +240,41 @@ class TrafikverketTrain(object):
         return TrainStop.from_xml_node(train_announcement)
 
     async def async_get_next_train_stop(
-            self, from_station: StationInfo,
-            to_station: StationInfo,
-            after_time: datetime) -> TrainStop:
+        self, from_station: StationInfo, to_station: StationInfo, after_time: datetime
+    ) -> TrainStop:
         """Enable retreival of next departure."""
         date_as_text = after_time.strftime(Trafikverket.date_time_format)
 
-        filters = [FieldFilter(FilterOperation.equal,
-                               "ActivityType",
-                               "Avgang"),
-
-                   FieldFilter(FilterOperation.equal,
-                               "LocationSignature",
-                               from_station.signature),
-
-                   FieldFilter(FilterOperation.greater_than_equal,
-                               "AdvertisedTimeAtLocation",
-                               date_as_text),
-
-                   OrFilter([FieldFilter(FilterOperation.equal,
-                                         "ViaToLocation.LocationName",
-                                         to_station.signature),
-                             FieldFilter(FilterOperation.equal,
-                                         "ToLocation.LocationName",
-                                         to_station.signature)])]
+        filters = [
+            FieldFilter(FilterOperation.equal, "ActivityType", "Avgang"),
+            FieldFilter(
+                FilterOperation.equal, "LocationSignature", from_station.signature
+            ),
+            FieldFilter(
+                FilterOperation.greater_than_equal,
+                "AdvertisedTimeAtLocation",
+                date_as_text,
+            ),
+            OrFilter(
+                [
+                    FieldFilter(
+                        FilterOperation.equal,
+                        "ViaToLocation.LocationName",
+                        to_station.signature,
+                    ),
+                    FieldFilter(
+                        FilterOperation.equal,
+                        "ToLocation.LocationName",
+                        to_station.signature,
+                    ),
+                ]
+            ),
+        ]
 
         sorting = [FieldSort("AdvertisedTimeAtLocation", SortOrder.ascending)]
         train_announcements = await self._api.async_make_request(
-            "TrainAnnouncement",
-            "1.6",
-            TrainStop._required_fields,
-            filters,
-            1,
-            sorting)
+            "TrainAnnouncement", "1.6", TrainStop._required_fields, filters, 1, sorting
+        )
 
         if len(train_announcements) == 0:
             raise ValueError("No TrainAnnouncement found")
