a3da711293e7b87ca964454fce846ee94009f32f
G Johansson <goran.johansson@shiftit.se>
2022-10-08 11:46:28 +0200

    Add camera connection

--- pytrafikverket/__init__.py
+++ pytrafikverket/__init__.py
@@ -1,11 +1,26 @@
 """Pytrafikverket module."""
 # flake8: noqa
-from pytrafikverket.trafikverket import (AndFilter, FieldFilter, FieldSort,
-                                         Filter, FilterOperation, NodeHelper,
-                                         OrFilter, SortOrder, Trafikverket)
-from pytrafikverket.trafikverket_train import (StationInfo, TrafikverketTrain,
-                                               TrainStop, TrainStopStatus)
-from pytrafikverket.trafikverket_weather import (TrafikverketWeather,
-                                                 WeatherStationInfo)
-from pytrafikverket.trafikverket_ferry import (TrafikverketFerry,
-                                               FerryStop, FerryStopStatus)
+from pytrafikverket.trafikverket import (
+    AndFilter,
+    FieldFilter,
+    FieldSort,
+    Filter,
+    FilterOperation,
+    NodeHelper,
+    OrFilter,
+    SortOrder,
+    Trafikverket,
+)
+from pytrafikverket.trafikverket_train import (
+    StationInfo,
+    TrafikverketTrain,
+    TrainStop,
+    TrainStopStatus,
+)
+from pytrafikverket.trafikverket_weather import TrafikverketWeather, WeatherStationInfo
+from pytrafikverket.trafikverket_ferry import (
+    TrafikverketFerry,
+    FerryStop,
+    FerryStopStatus,
+)
+from pytrafikverket.trafikverket_camera import TrafikverketCamera, CameraInfo
